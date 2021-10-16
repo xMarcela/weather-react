ad20185e4f2e6e6107a82ff0189ea64c4ba47f12
xMarcela <58213158+xMarcela@users.noreply.github.com>
2021-10-16 17:13:27 +0200

    Add axios intergration to display weather of a city
    Quick weather search made with React

---
+++ src/Weather.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp} C`);
+    }
+
+    let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return (
+        <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={3000} />
+    );
+}
